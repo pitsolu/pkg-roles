3bea762fda16820e2fe06def63242fa1caa9443e
pitsolu <pitsolu@gmail.com>
2020-08-24 14:43:43 +0300

    minor refactor

--- package/database/schema/Schema/Migration/VersionRoles.php
+++ package/database/schema/Schema/Migration/VersionRoles.php
@@ -8,7 +8,7 @@ use Doctrine\DBAL\Schema\Schema;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-class VersionAuth extends AbstractMigration
+class VersionRoles extends AbstractMigration
 {
     /**
      * @param Schema $schema
